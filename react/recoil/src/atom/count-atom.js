df92db5b2be3b8afcbb27665b24f3d6fe3c4e10e
mw <minwook0523@gmail.com>
2023-12-19 22:13:25 +0900

    selector family 예제

--- react/recoil/src/atom/count-atom.js
+++ react/recoil/src/atom/count-atom.js
@@ -1,9 +1,25 @@
-import { atom,selector } from "recoil"
+import { atom,selector, selectorFamily } from "recoil"
 export const countState = atom({
   key: "count",
   default: 0
 })
 
+export const numberState = atom({
+  key : 'numberTest',
+  default : 1
+})
+
+export const numberSelector = selectorFamily({
+  key : 'numberSelector',
+  get : (value) =>({get}) =>{
+    return get(countState) * value
+  },
+  set : (value) =>({get,set}, newValue) =>{
+    let test = get(countState)
+    let test2 = test * newValue * value // newValue :setter 함수로 입력받아온 값, value : 컴포넌트에서 매개변수로 넣은 값
+    set(numberState, test2)
+  }
+})
 // export const countSelector = selector({
 //   key: 'test',
 //   get : ({get}) =>{
--- react/recoil/src/components/Counter.js
+++ react/recoil/src/components/Counter.js
@@ -1,11 +1,14 @@
-import { useRecoilState } from 'recoil';
-import { countState } from '../atom/count-atom';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { countState, numberSelector, numberState } from '../atom/count-atom';
 export default function Counter() {
 
   const [count, setCount] = useRecoilState(countState)
+  const [number, setNumber] = useRecoilState(numberState)
+  const [number2, setNumber2] = useRecoilState(numberSelector(100))
   const click = (num) =>{
     setCount(count + num)
   }
+
   return (
     <div>
       <h1>Counter</h1>
@@ -16,6 +19,12 @@ export default function Counter() {
       <button onClick={()=>{click(-1)}}>
         -
       </button>
+
+      <p>Selector Family Test</p>
+    <p>number atom : {number}</p>
+    <p>selector : {number2}</p>
+    <button onClick={()=>{setNumber2(10)}}>set 버튼</button>
+    <button onClick={()=>{setNumber(count)}}>number atom  버튼</button>
     </div>
   );
 }
