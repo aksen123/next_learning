17010cfe616c65033ae65e3385116432d15f771d
mw <minwook0523@gmail.com>
2024-01-13 12:27:04 +0900

    js 한바퀴 돌기~

---
+++ JS/JS.js
@@ -0,0 +1,9 @@
+
+let test1 =document.querySelector('.box1')
+
+let test2 =document.querySelector('.box1 p')
+
+function boxShow(text1,text2){
+  test2.innerHTML = text2
+  test1.style.display = text1
+}
